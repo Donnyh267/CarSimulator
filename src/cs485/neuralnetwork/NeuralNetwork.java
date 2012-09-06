e777788da96eeecc4b053530e823df8604d36fe5
Donny <Donnyh267@gmail.com>
2012-09-06 16:19:05 -0400

    Save and Load Neural Network

--- src/cs485/neuralnetwork/NeuralNetwork.java
+++ src/cs485/neuralnetwork/NeuralNetwork.java
@@ -1,6 +1,7 @@
 package cs485.neuralnetwork;
 
 import java.awt.List;
+import java.io.File;
 import java.util.ArrayList;
 
 import org.encog.ml.data.MLData;
@@ -10,14 +11,17 @@ import org.encog.neural.data.NeuralData;
 import org.encog.neural.data.basic.BasicNeuralData;
 import org.encog.neural.networks.BasicNetwork;
 import org.encog.neural.networks.layers.BasicLayer;
+import org.encog.persist.EncogDirectoryPersistence;
 
 import scr.Action;
 import scr.SensorModel;
 
 public class NeuralNetwork {
 	
+	private final String FILENAME = "network.eg";
 	private BasicNetwork network = new BasicNetwork();
 	
+	//Build & Initialize the Network
 	public void initialize() {
 		network.addLayer(new BasicLayer(28));	//Input Layer
 		network.addLayer(new BasicLayer(10));	//Hidden Layer
@@ -29,9 +33,33 @@ public class NeuralNetwork {
 	//trainer function
 	public void train() {
 		
+		this.save();
 	}
 	
+	//Get Action based upon SensorModel
 	public Action getAction(SensorModel sensors) {
+		
+		
+		//Enter sensor Data into Neural Network
+		BasicNeuralData input = sensorToInput(sensors);
+		BasicNeuralData output = (BasicNeuralData) network.compute(input);
+		
+		//Create Action
+		Action newAction = new Action();
+		newAction.accelerate = output.getData(0);
+		newAction.brake = output.getData(1);
+		newAction.clutch = output.getData(2);
+		newAction.gear = (int) output.getData(3);
+		newAction.steering = output.getData(4);
+		if (output.getData(5) < 0.50 && output.getData(5) >= 0)
+			newAction.restartRace = true;
+		else
+			newAction.restartRace = false;
+		newAction.focus = (int) output.getData(6);
+		return newAction;
+	}
+	
+	public static BasicNeuralData sensorToInput(SensorModel sensors) {
 		//Fill sensor Data
 		ArrayList<Double> inputList = new ArrayList<Double>();
 		inputList.add(sensors.getSpeed());	//Speed
@@ -47,23 +75,17 @@ public class NeuralNetwork {
 		for (int i = 0; i < inputList.size(); i++)
 			inputArray[i] = inputList.get(i);
 		
-		//Enter sensor Data into Neural Network
-		MLData input = (MLData) new BasicNeuralData(inputArray);
-		MLData output = network.compute(input);
-		
-		//Create Action
-		Action newAction = new Action();
-		newAction.accelerate = output.getData(0);
-		newAction.brake = output.getData(1);
-		newAction.clutch = output.getData(2);
-		newAction.gear = (int) output.getData(3);
-		newAction.steering = output.getData(4);
-		if (output.getData(5) < 0.50 && output.getData(5) >= 0)
-			newAction.restartRace = true;
-		else
-			newAction.restartRace = false;
-		newAction.focus = (int) output.getData(6);
-		return newAction;
+		BasicNeuralData input = new BasicNeuralData(inputArray);
+		return input;
+	}
+	
+	
+	public void save() {
+		EncogDirectoryPersistence.saveObject(new File(FILENAME), this.network);
+	}
+	
+	public void load() {
+		network = (BasicNetwork)EncogDirectoryPersistence.loadObject(new File(FILENAME));
 	}
 
 }
